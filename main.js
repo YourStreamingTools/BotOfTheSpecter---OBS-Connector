92db6f9584c5a3c796caf30a28c01be05c9c81c0
gfaUnDead <contact@gfaundead.stream>
2024-09-24 22:16:20 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-    },
-});
-
-win.loadFile('index.html');
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
