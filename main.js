342c886202ad1773c0424cc8816092cf021f8282
gfaUnDead <contact@gfaundead.stream>
2024-09-24 23:17:43 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const path = require('path');
 // Disable hardware acceleration
 app.disableHardwareAcceleration();
 
+// Additional command-line switches to disable GPU features
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-gpu-compositing');
+app.commandLine.appendSwitch('disable-d3d11');
+app.commandLine.appendSwitch('disable-d3d11-debug-layer');
+app.commandLine.appendSwitch('disable-es3-gl-context');
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
